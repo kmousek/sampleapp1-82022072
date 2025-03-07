56a2ed53bf659f9cb6a6cb2fef0467a75cec1523
kmousek <48666998+kmousek@users.noreply.github.com>
2025-03-07 10:10:08 +0900

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,1 +1,14 @@
 
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TodoController {
+
+    @GetMapping("/user")
+    public String home() {
+        return "82022072";
+    }
+}
