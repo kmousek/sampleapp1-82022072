dfbdff76951b9b18f13c6998fb1b2aed722d14b6
kmousek <kmousek@naver.com>
2025-03-07 10:30:29 +0900

    commit java version and usercontroller

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,13 +1,15 @@
 
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class TodoController {
+@RequestMapping("/api/user")
+@RestController
+public class UserController {
 
-    @GetMapping("/user")
+	@GetMapping
     public String home() {
         return "82022072";
     }
